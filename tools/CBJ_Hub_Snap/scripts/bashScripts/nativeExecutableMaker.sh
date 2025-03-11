3d8a91ccc126e70eac835ade2efd9281503770d2
guyluz11 <guyluz11@gmail.com>
2025-03-11 13:22:39 +0200

    Removed dump from snapcraft as this is now a monorepo. Improved readability and maintenance of snapcraft with nativeExecutableMaker.sh for all dart projects

---
+++ tools/CBJ_Hub_Snap/scripts/bashScripts/nativeExecutableMaker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Making native executable of the project"
+
+PROJECT_PATH="$1"
+MAIN_FILE="$2"
+EXECUTABLE_NAME ="$3"
+
+if [[ -z "$PROJECT_PATH" || -z "$MAIN_FILE" || -z "$EXECUTABLE_NAME" ]]; then
+    echo "Error: Missing arguments!"
+    echo "Usage: ./nativeExecutableMaker.sh <PROJECT_PATH> <ANOTHER_VAR>"
+    exit 1
+fi
+
+scripts/bashScripts/dartSdkDownload.sh # Downloading dart-sdk for the correct architecture.
+
+unzip dartsdk-*.zip
+rm dartsdk-*.zip
+
+dart-sdk/bin/dart pub get --no-precompile --directory="$PROJECT_PATH"
+dart-sdk/bin/dart run build_runner build --delete-conflicting-outputs --directory="$PROJECT_PATH"
+
+dart-sdk/bin/dart compile exe "$PROJECT_PATH/$MAIN_FILE" -o "$EXECUTABLE_NAME"
+
+rm -r dart-sdk/
