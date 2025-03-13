f7dd7fa72c2ae3f3afa4cca68d1daf1aee90fa10
Guy Luz <guyluz11@gmail.com>
2025-03-13 21:12:36 +0200

    Snap getting built successfully

--- tools/CBJ_Hub_Snap/scripts/bashScripts/nativeExecutableMaker.sh
+++ tools/CBJ_Hub_Snap/scripts/bashScripts/nativeExecutableMaker.sh
@@ -5,6 +5,7 @@ echo "Making native executable of the project"
 PROJECT_PATH="$1"
 MAIN_FILE="$2"
 EXECUTABLE_NAME="$3"
+CURRENT_PATH="$HOME/project/"
 
 if [[ -z "$PROJECT_PATH" || -z "$MAIN_FILE" || -z "$EXECUTABLE_NAME" ]]; then
     echo "Error: Missing arguments!"
@@ -23,8 +24,12 @@ rm -f "$PROJECT_PATH/pubspec.lock"
 
 dart-sdk/bin/dart pub get --no-precompile --directory="$PROJECT_PATH"
 dart-sdk/bin/dart run build_runner clean --directory="$PROJECT_PATH"
-dart-sdk/bin/dart run build_runner build --delete-conflicting-outputs --directory="$PROJECT_PATH"
 
+cd "$PROJECT_PATH"
+# Found a bug where "--directory" does not work with "dart run build_runner"
+#dart-sdk/bin/dart run build_runner build --delete-conflicting-outputs --directory="$PROJECT_PATH"
+$CURRENT_PATH/dart-sdk/bin/dart run build_runner build --delete-conflicting-outputs
+cd "$CURRENT_PATH"
 dart-sdk/bin/dart compile exe "$PROJECT_PATH/$MAIN_FILE" -o "$EXECUTABLE_NAME"
 
 rm -r dart-sdk/
