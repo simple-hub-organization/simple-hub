e48ddd8eb6dc16d11e20637deb501744fe343bbe
guyluz11 <guyluz11@gmail.com>
2025-03-23 12:31:51 +0200

    Working on releasing the app

--- apps/app/windows/flutter/generated_plugin_registrant.cc
+++ apps/app/windows/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,7 @@
 #include <bonsoir_windows/bonsoir_windows_plugin_c_api.h>
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <file_selector_windows/file_selector_windows.h>
+#include <flutter_timezone/flutter_timezone_plugin_c_api.h>
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 #include <nsd_windows/nsd_windows_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
@@ -21,6 +22,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterTimezonePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterTimezonePluginCApi"));
   IsarFlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
   NsdWindowsPluginCApiRegisterWithRegistrar(
--- apps/app/windows/flutter/generated_plugins.cmake
+++ apps/app/windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   bonsoir_windows
   connectivity_plus
   file_selector_windows
+  flutter_timezone
   isar_flutter_libs
   nsd_windows
   permission_handler_windows
