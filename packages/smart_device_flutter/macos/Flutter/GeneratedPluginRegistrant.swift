def024f386e206aacb78a06c163ec7cb83564c4a
Guy Luz <guyluz11@gmail.com>
2025-04-07 20:33:12 +0300

    wip run on iOS

--- packages/smart_device_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/smart_device_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import nsd_macos
 import package_info_plus
@@ -12,6 +13,7 @@ import path_provider_foundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   NsdMacosPlugin.register(with: registry.registrar(forPlugin: "NsdMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
